16b59848ffcf7173aebcd81443ffdba1e2185342
zyy2019 <zyy2019@126.com>
2018-12-28 17:34:31 +0800

    增加发送带附件的邮件方法

--- run.py
+++ run.py
@@ -8,11 +8,11 @@ import unittest
 from unittest import TextTestRunner
 
 from util.createReport import run_and_createReport
-from util.sendEmail import send_email
+from util.sendEmail import send_email, send_email_attach
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover("./testcase", pattern="*Test.py")
     # TextTestRunner().run(suite)
     file_name=run_and_createReport(suite)
-    send_email(file_name)
+    send_email_attach(file_name)
 
--- util/sendEmail.py
+++ util/sendEmail.py
@@ -4,11 +4,15 @@
 # @Site    : 
 # @File    : sendEmail.py
 # @Software: PyCharm
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP, SMTP_SSL
 
 # 邮件设置信息
 from util.readFile import read_csv
+
+
 
 email_info={
     "host":"smtp.126.com",
@@ -21,6 +25,44 @@ email_info={
     "mailSubject":"Qunar机票搜索测试报告",
     "mailEncoding":"utf-8"
 }
+
+
+def send_email_attach(file_name):
+    to_addrs = getAllReciver()
+
+    # 读取要发送的邮件内容
+
+    file = open(file_name, 'rb')
+    file_content = file.read()
+    file.close()
+
+    # 带附件的实例
+    mime =MIMEMultipart()
+    mime["Subject"] = email_info["mailSubject"]
+    mime["From"] = email_info["fromAddr"]
+    mime["To"] = ",".join(to_addrs)
+
+    # 邮件正文
+    mime.attach(MIMEText(file_content, _subtype=email_info["mailType"], _charset=email_info["mailEncoding"]))
+
+    # 附件1
+    html_attachment = MIMEText(open(file_name, 'rb').read(), 'base64', 'utf-8')
+    html_attachment["Content-Type"] = 'application/octet-stream'
+    html_attachment["Content-Disposition"] = 'attachment; filename="test_port.html"'
+    mime.attach(html_attachment)
+    # 附件2
+    img_attachment = MIMEText(open(file_name, 'rb').read(), 'base64', 'utf-8')
+
+    # imageFile = '1.png'
+    # imageApart = MIMEImage(open(imageFile, 'rb').read(), imageFile.split('.')[-1])
+    # imageApart.add_header('Content-Disposition', 'attachment', filename=imageFile)
+    smtp= SMTP_SSL(email_info["host"],email_info["port"])
+    smtp.login(email_info["username"], email_info["password"])
+    smtp.sendmail(email_info["fromAddr"], to_addrs, mime.as_string())
+    smtp.close()
+
+    print("邮件发送成功！")
+
 
 def send_email(file_name):
     print("邮件设置参数")
